42a20957b1d71dadf7885670289e2fda5dbae831
Karolina Dudkowska <karoladudkowska@wp.pl>
2021-03-08 19:03:31 +0100

    validation

--- src/Signup.js
+++ src/Signup.js
@@ -13,11 +13,63 @@ class Signup extends Component {
             email: '',
             password: '',
             passwordRep: '',
-            correctValidation: true
+            nameValidation: true,
+            emailValidation: true,
+            passwordValidation: true,
+            passwordRepValidation: true,
         };
 
     }
 
+
+    validation = () => {
+
+        console.log('waliduje');
+        let validationCorrect = true;
+        let validationEmailCorrect = true;
+        let validationPasswordCorrect = true;
+        let validationPasswordRep = true;
+
+        if (this.state.name === '') {
+            this.setState({ nameValidation: false });
+            validationCorrect = false;
+        } else {
+            this.setState({ nameValidation: true });
+            validationCorrect = true;
+        }
+
+        if (this.state.email === '') {
+            this.setState({ emailValidation: false });
+            validationEmailCorrect = false;
+        } else {
+            this.setState({ emailValidation: true });
+            validationEmailCorrect = true;
+        }
+
+        if (this.state.password === '') {
+            this.setState({ passwordValidation: false });
+            validationPasswordCorrect = false;
+        } else {
+            this.setState({ passwordValidation: true });
+            validationPasswordCorrect = true;
+        }
+
+        if (this.state.password === this.state.passwordRep) {
+            this.setState({ passwordRepValidation: true });
+            validationPasswordRep = true;
+        } else {
+            this.setState({ passwordRepValidation: false });
+            validationPasswordRep = false;
+        }
+
+        return validationCorrect
+
+        return validationEmailCorrect
+
+        return validationPasswordCorrect
+
+        return validationPasswordRep;
+    }
 
 
 
@@ -25,35 +77,20 @@ class Signup extends Component {
         event.preventDefault();
         console.log(`this sigUser`);
 
-        if (!this.correctValidation) {
+        if (!this.validation()) {
             return;
         }
-        // const user = {
-        //     name: this.state.name
-        // };
-
-        // axios.post(
-        //     'http://akademia108.pl/api/social-app/user/signup',
-        //     JSON.stringify(newUser),
-        //     { 'headers': headers })
-        //     .then((req) => {
-
-        //         // your code :)      
-
-        //         console.log(req.data);
-        //     }).catch((error) => {
-        //         console.error(error);
-        //     })
 
         let newUser = {
             username: this.state.name,
             email: this.state.email,
-            password: this.state.password,
+            password: this.state.password
         }
 
         const headers = {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+
         }
 
         axios.post(
@@ -63,6 +100,8 @@ class Signup extends Component {
             .then((req) => {
 
                 // your code :)   waidacja ze jest już usrname w bazie   
+
+
 
                 console.log(req.data);
             }).catch((error) => {
@@ -110,22 +149,20 @@ class Signup extends Component {
                     <input type="text" name="name" id="" value={this.state.name} onChange={this.handleNameChange}>
 
                     </input>
-                    <p>Wpisz nazwę uzytkownika</p>
+                    {!this.state.nameValidation && <p>Wpisz nazwę uzytkownika</p>}
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleEmailChange}
                     ></input>
-                    <p>Wpisz email</p>
+                    {!this.state.emailValidation && <p>Wpisz email</p>}
                     <label for="password">Hasło</label>
-                    <input type="password" name="password" id="password"
-                        onChange={this.handlePasswordChange}
+                    <input type="password" name="password" id="password" onChange={this.handlePasswordChange}
                     ></input>
-                    <p>Wpisz hasło</p>
+                    {!this.state.passwordValidation && <p>Wpisz hasło</p>}
                     <label for="password">Potwierdzenie hasła</label>
-                    <input type="password" name="passwordReps" id="passwordRep" value={this.state.passwordRep}
-
-                        onChange={this.handlePasswordRepChange}
+                    <input type="password" name="passwordRep" id="passwordRep" value={this.state.passwordRep} onChange={this.handlePasswordRepChange}
                     ></input>
-                    <p>Powtórz hasło</p>
+                    {!this.state.passwordValidation && <p>Powtórz hasło</p>}
+                    {!this.state.passwordRepValidation && <p>Hasło musi byc identyczne jak w polu Hasło</p>}
                     <input className="submit" type="submit" value="Wyślij"></input>
                 </form >
             </div >
